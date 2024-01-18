f48c3210a319d961a28e2b2b4b8d0fcb17f987f7
vivek <vivekjeevanarajh@gmail.com>
2024-01-18 08:24:06 +0530

    doc ayt

---
+++ pages/document.js
@@ -0,0 +1,22 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Add your meta tags or other head elements here */}
+          <meta name="description" content="Your meta description" />
+          {/* Other meta tags or elements go here */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
